aa960189e090e46d8766c15f40ccb89c2efec85d
Chen Xiaohui <graych@gmail.com>
2017-07-30 16:39:06 +0800

    -

--- install-npc-shell.sh
+++ install-npc-shell.sh
@@ -2,25 +2,29 @@
 
 # NPC_INSTALL_ACTION=deploy ./install-npc-shell.sh
 deploy(){
-	tar -zcf npc-shell.tar.gz npc-shell.sh npc-setup.sh && npc nos PUT /npc/dl/npc-shell.tar.gz @npc-shell.tar.gz \
-		&& rm -f npc-shell.tar.gz && npc nos PUT /npc/dl/install-npc-shell.sh @install-npc-shell.sh \
+	[ -x ./npc-shell.sh ] && [ -f ./install-npc-shell.sh ] \
+		&& tar -zcf npc-shell.tar.gz npc-shell.sh && ./npc-shell.sh nos PUT /npc/dl/npc-shell.tar.gz @npc-shell.tar.gz \
+		&& rm -f npc-shell.tar.gz && ./npc-shell.sh nos PUT /npc/dl/install-npc-shell.sh @install-npc-shell.sh \
 		&& echo 'Deployed' && return 0
 	return 1
 }
 
 install(){
 	# https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64
-	curl 'http://npc.nos-eastchina1.126.net/dl/jq_1.5_linux_amd64.tar.gz' |  tar -zx -C '/usr/local/bin' \
-		|| rm -f /usr/local/bin/jq
+	curl 'http://npc.nos-eastchina1.126.net/dl/jq_1.5_linux_amd64.tar.gz' |  tar -zx -C '/usr/bin' || {
+		rm -f /usr/bin/jq
+		return 1
+	}
 	# https://github.com/xiaopal/npc-shell
-	curl 'http://npc.nos-eastchina1.126.net/dl/npc-shell.tar.gz' | tar -zx -C '/usr/local/bin' \
-		&& ln -sf /usr/local/bin/npc-shell.sh /usr/local/bin/npc
-
-	[ -x /usr/local/bin/jq ] && [ -x /usr/local/bin/npc-shell.sh ] && {
+	mkdir -p /usr/npc-shell \
+		&& curl 'http://npc.nos-eastchina1.126.net/dl/npc-shell.tar.gz' | tar -zx -C '/usr/npc-shell' \
+		&& ln -sf /usr/npc-shell/npc-shell.sh /usr/bin/npc && {
 		echo "Installed" && npc
 		return 0
 	}
 	return 1
 }
-
-"${NPC_INSTALL_ACTION:-install}"
+[ "$1" == "deploy" ] && {
+	deploy || exit 1
+}
+install
